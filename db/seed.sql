5bfa2e269362a8212956cd173fc5b614343e9136
Angel Rios <riosmartinezkg@gmail.com>
2026-04-22 20:47:37 -0600

    Feat: schema.sql and seed.sql added to db/

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO friend_request_status (name) VALUES ('pending');
+INSERT INTO friend_request_status (name) VALUES ('accepted');
+INSERT INTO friend_request_status (name) VALUES ('rejected');
