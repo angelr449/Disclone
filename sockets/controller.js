2beaefae243e57869ddff87bb923b4a345faca47
Angel Rios <riosmartinezkg@gmail.com>
2026-06-19 22:10:08 -0600

    fix: switch soket auth from headers to handshake auth token

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,50 +1,32 @@
 const { validateSocketJWT } = require("../middlewares/validate-jwt");
 const { Message } = require("../models");
 
-
-
-
-
-
 const socketController = async (socket, io) => {
+    const token = socket.handshake.auth.token;
 
-    const user = await validateSocketJWT(socket.handshake.headers['x-token']);
+    const user = await validateSocketJWT(token);
 
     if (!user) {
         return socket.disconnect();
     }
 
-    // Join a chat
-    socket.on('joinChat', (chatId)=>{
+    socket.on('joinChat', (chatId) => {
         socket.join(chatId);
-    })
+    });
 
-    // Send message
-
-   socket.on('sendMessage', async ({ chatId, content }) => {
-    try {
-        if (!chatId || !content?.trim()) return;
-
-        const newMessage = await Message.create({
-            chat_id: chatId,
-            sender_id: user.id,
-            content: content.trim()
-        });
-
-        io.to(chatId).emit('newMessage', newMessage);
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-   
-
-
-}
-
-
-
+    socket.on('sendMessage', async ({ chatId, content }) => {
+        try {
+            const newMessage = await Message.create({
+                chat_id: chatId,
+                sender_id: user.id,
+                content
+            });
+            io.to(chatId).emit('newMessage', newMessage);
+        } catch (error) {
+        }
+    });
+};
 
 module.exports = {
     socketController
-}
+};
